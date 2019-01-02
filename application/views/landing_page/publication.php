4b05fd29e2bba9a225623d9a2b679ae6748292a0
firdasafridi <firdasafridi@ymail.com>
2019-01-02 11:59:53 +0700

    Page publikasi

--- application/views/landing_page/publication.php
+++ application/views/landing_page/publication.php
@@ -17,10 +17,10 @@
                 <div class="row">
                     <div class="col-md-12">
                        <div class="title-section">
-                            <h1 class="title">The Areas of Practice</h1>
-                            <div class="sub-title">
+                            <h1 class="title">Daftar Publikasi</h1>
+                            <!-- <div class="sub-title">
                                 Simply looking for a pretty website? We're much more.
-                            </div>
+                            </div> -->
                         </div> 
                     </div>
                 </div>
@@ -31,14 +31,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p1.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p1.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Strategic Planning</a></div> 
+                                <div class="box-title"><a href="#">Perencanaan Strategis</a></div> 
                             </div>
                             <div class="box-content">
                                 <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
@@ -47,14 +47,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p2.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p2.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Trades & Stocks</a></div> 
+                                <div class="box-title"><a href="#">Perdagangan & Saham</a></div> 
                             </div>
                             <div class="box-content">
                                 <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
@@ -63,14 +63,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p3.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p3.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Audit & Assurance</a></div> 
+                                <div class="box-title"><a href="#">Audit & Asuransi</a></div> 
                             </div>
                             <div class="box-content">
-                                <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
+                                <p>Nemo enim ipsam volup tatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
@@ -82,14 +82,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p4.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p4.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Turnaround Consulting</a></div> 
+                                <div class="box-title"><a href="#">Konsultasi Perputaran</a></div> 
                             </div>
                             <div class="box-content">
                                 <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
@@ -98,14 +98,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p5.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p5.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Financial Projections</a></div> 
+                                <div class="box-title"><a href="#">Proyeksi Keuangan</a></div> 
                             </div>
                             <div class="box-content">
                                 <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
@@ -114,14 +114,14 @@
                         <div class="imgbox style2 position-title">
                             <div class="box-header">
                                 <div class="box-img">
-                                    <a href="services-single.html"><img src="<?=base_url()?>probono_asset/images/services/p6.jpg" alt="image"></a>
+                                    <a href="#"><img src="<?=base_url()?>probono_asset/images/services/p6.jpg" alt="image"></a>
                                 </div>
-                                <div class="box-title"><a href="services-single.html">Financial Analysis</a></div> 
+                                <div class="box-title"><a href="#">Anlisis Finansial</a></div> 
                             </div>
                             <div class="box-content">
                                 <p>Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit sed quia consequuntur.</p>
                                 <div class="explore">
-                                    <a class="read-more" href="services-single.html" title="">Read More</a>
+                                    <a class="read-more" href="#" title="">Baca Selengkapnya</a>
                                 </div> 
                             </div>
                         </div>
