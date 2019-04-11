af3d68fc67b307036f931bcf7e99e1adf2e410c5
firdasafridi <firdasafridi@ymail.com>
2019-04-11 20:56:47 +0700

    Revert "Merge pull request #38 from imamfchrj/dev"
    This reverts commit 80a3ae53fd88c044cf1ac0787a120d9f28df7b92, reversing
    changes made to 136246488de60580ab419d869086a6df701ddc69.

--- application/views/landing_page/team.php
+++ application/views/landing_page/team.php
@@ -1,16 +1,6 @@
 <?php $this->load->view('landing_page/layout/header')?>
 
     <main class="main-content" id="main-content">
-
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-        <script type="text/javascript" src="//gitcdn.link/repo/Lwangaman/jQuery-Clock-Plugin/master/jqClock.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function(){
-                customtimestamp = parseInt($("#jqclock").data("time"));
-                $("#jqclock").clock({"langSet":"en","timestamp":customtimestamp});
-                $("#jqclock-local").clock({"langSet":"en"});
-            });
-        </script>
         
         <!-- Services -->
         <section class="flat-row section-services pd-bottom6">
@@ -20,37 +10,25 @@
                         <div class="wrap-teammember">
                             <div class="flat-team team-list style2 clearfix">
                                 <div class="avatar"> 
-                                    <a href="#" class="opacity" >
-                                        <img src="<?=base_url()?>probono_asset/images/Mappi.png" alt="image">
+                                    <a href="#" class="opacity">
+                                        <img src="<?=base_url()?>probono_asset/images/clients/2.jpg" alt="image">
                                     </a>  
                                 </div>                        
                                 <div class="content">
-                                    <!--<span class="position">Founder & CEO</span>-->
-                                    <h5 class="name">MAPPI UI</h5>
-                                    <p>Kami adalah Masyarakat Pemantau Peradilan Indonesia (MaPPI) Fakultas Hukum Universitas Indonesia. Kami merupakan lembaga kajian dan penelitian di bawah Fakultas Hukum Universitas Indonesia yang bergerak dalam bidang Penelitian dan Advokasi Peradilan. Kami merupakan lembaga yang bersifat independen dan profesional.</p>
-                                    <h6 class="name">VISI</h6>
-                                    <p>Visi kami adalah mewujudkan peradilan dan kebijakan hukum yang adil serta rasional untuk setiap umat manusia.</p>
-                                    <h6 class="name">MISI</h6>
-                                    <p>Untuk mewujudkan visi itu, kami berupaya untuk:<br>
-                                        1.	Menyediakan penelitian berbasis bukti yang dapat dipercaya dan bermutu;<br>
-                                        2.	Mendukung pemangku kepentingan terkait isu-isu strategis; dan<br>
-                                        3.	Melakukan otoritas pemberdayaan masyarakat demi kepentingan masyarakat dan ilmu pengetahuan.
-                                    </p>
-                                    <h6 class="name">Kontak</h6>
+                                    <span class="position">Founder & CEO</span>
+                                    <h6 class="name">MAPPI UI</h6>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem santium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.</p>
                                     <ul class="info-team">
-                                        <li><span>Alamat:</span>Fakultas Hukum Universitas Indonesia, Gedung D, Lantai 4
-                                        </li>
-                                        <li><span></span>Ruang 404 Kampus UI Depok 16424</li>
-                                        <li><span>Email:</span>office@mappifhui.org</li>
+                                        <li><span>Telpon:</span>001-1234-88888</li>
+                                        <li><span>Email:</span>mappiui@gmail.com</li>
+                                        <li><span>Pengalaman:</span>5 years</li>
                                         <li class="team-social"><span>Social:</span>
-                                        <a href="http://www.facebook.com/mappifhui" class="facebook"><i class="fa fa-facebook"></i></a>
-                                        <a href="http://twitter.com/mappifhui" class="twitter"><i class="fa fa-twitter"></i></a>
-                                        <a href="http://instagram.com/mappifhui" class="instagram"><i class="fa fa-instagram"></i></a>
+                                        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                                        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                                        <a href="#" class="printer"><i class="fa fa-pinterest-p"></i></a>
+                                        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
                                         </li>
                                     </ul>
-                                        <div id="jqclock" class="jqclock" data-time="<?php echo time(); ?>"></div>
-                                        <!--//local time
-                                        <div id="jqclock-local" class="jqclock"></div>-->
                                 </div>
                             </div>
                         </div>  
